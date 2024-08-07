d34d539e8bcbb93e680590d94a96fb4ef71dd4cf
Youssef Mohamed <joe375649@gmail.com>
2024-08-08 00:52:13 +0300

    added signup models and repo

--- lib/features/auth/data/repos/auth_repo.dart
+++ lib/features/auth/data/repos/auth_repo.dart
@@ -1,7 +1,10 @@
 import 'package:auvnet_store/core/service/networking/api_result.dart';
 import 'package:auvnet_store/core/service/networking/api_service.dart';
+import 'package:auvnet_store/core/utils/app_strings.dart';
 import 'package:auvnet_store/features/auth/data/models/login_request_body.dart';
 import 'package:auvnet_store/features/auth/data/models/login_response.dart';
+import 'package:auvnet_store/features/auth/data/models/sign_up_request_body.dart';
+import 'package:auvnet_store/features/auth/data/models/signup_response.dart';
 
 class AuthRepos {
   const AuthRepos(this._apiService);
@@ -13,8 +16,18 @@ class AuthRepos {
 
       return ApiResult.success(response);
     } catch (error) {
-      print('errrror====>' + error.toString());
-      return  ApiResult.failure(error.toString());
+      return const ApiResult.failure(errorMessage);
+    }
+  }
+
+  //SignUp
+  Future<ApiResult<SignupResponse>> signUp(SignupRequestBody body) async {
+    try {
+      final response = await _apiService.signUp(body);
+
+      return ApiResult.success(response);
+    } catch (error) {
+      return const ApiResult.failure(errorMessage);
     }
   }
 }
