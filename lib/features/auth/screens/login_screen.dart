7d13a2a42eeffe2a03218a97dc0accfba8a0aa41
Youssef Mohamed <joe375649@gmail.com>
2024-08-07 16:25:57 +0300

    added custom linear buttom added custom text form fiels added extension file added login ui and the files of it

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -1,17 +1,35 @@
+import 'package:auvnet_store/core/extensions/context_extension.dart';
+import 'package:auvnet_store/features/auth/refactors/auth_custom_painter.dart';
+import 'package:auvnet_store/features/auth/refactors/login_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar:  AppBar(
-        title: Text('Login'),
+    return Scaffold(
+      bottomNavigationBar: CustomPaint(
+        size: Size(
+          MediaQuery.of(context).size.width,
+          150.h,
+        ),
+        painter: AuthCustomPainter(
+          gradient: LinearGradient(
+            colors: [
+              context.color.bluePinkLight!,
+              context.color.bluePinkLight!,
+              context.color.bluePinkLight!,
+              context.color.bluePinkDark!,
+            ],
+          ),
+        ),
       ),
-      body: Text('Login Screen'),
+      body: const SafeArea(
+        bottom: false,
+        child: LoginBody(),
+      ),
     );
-
-
   }
 }
