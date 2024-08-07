137de0f46326765412c52ad06467811402ef5217
Youssef Mohamed <joe375649@gmail.com>
2024-08-07 17:08:49 +0300

    added signup screen  added sign up widgets  added sign up body  added user avatar image for sign up

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -2,6 +2,7 @@ import 'package:auvnet_store/core/common/screens/under_build_screen.dart';
 import 'package:auvnet_store/core/routes/base_routes.dart';
 import 'package:auvnet_store/core/routes/routers.dart';
 import 'package:auvnet_store/features/auth/screens/login_screen.dart';
+import 'package:auvnet_store/features/auth/screens/sign_up_screen.dart';
 import 'package:flutter/material.dart';
 
 /// Handles route generation for the app
@@ -13,6 +14,11 @@ class AppRouter {
         // Route for the login screen
         return BaseRoute(
           page: const LoginScreen(),
+        );
+        case Routes.signUp:
+        // Route for the login screen
+        return BaseRoute(
+          page: const SignUpScreen(),
         );
 
       default:
---
+++ lib/features/auth/widgets/sign_up/sign_up_button.dart
@@ -0,0 +1,30 @@
+import 'package:auvnet_store/core/common/animations/animate_do.dart';
+import 'package:auvnet_store/core/common/widgets/custom_linear_button.dart';
+import 'package:auvnet_store/core/common/widgets/text_app.dart';
+import 'package:auvnet_store/core/extensions/context_extension.dart';
+import 'package:auvnet_store/core/styles/fonts/font_wieght_helper.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class SignUpButton extends StatelessWidget {
+  const SignUpButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomFadeInRight(
+      duration: 600,
+      child: CustomLinearButton(
+        onPressed: () {},
+        height: 50.h,
+        width: MediaQuery.of(context).size.width,
+        child: TextApp(
+          text: 'Sign Up',
+          theme: context.textStyle.copyWith(
+            fontSize: 18.sp,
+            fontWeight: FontWeightHelper.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
